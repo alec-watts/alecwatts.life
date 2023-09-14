e7478ccea719c97db93b8a1858a437e45a099609
alec-watts <alecwatts1@gmail.com>
2023-09-13 22:19:21 -0700

    yawn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 // import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Home2 from "./pages/Home2";
 
 function App() {
   return (
-    <Home />
+    // <Home />
+    <Home2 />
     // <Routes>
     //   <Route path="/" element={ <Home/> }/>
     //   <Route path="/support.html" element={ <Support/> }/>
---
+++ src/pages/Home2.tsx
@@ -0,0 +1,14 @@
+const Home2 = () => {
+  return (
+    <>
+      <div className="flex h-screen bg-blue-500 items-center justify-center">
+        <h1 className="text-9xl font-bold text-white uppercase">Alec Watts</h1>
+      </div>
+      <div className="flex h-screen bg-blue-500 items-center justify-center">
+        <h1 className="text-9xl font-bold text-white uppercase">Alec Watts</h1>
+      </div>
+    </>
+  );
+};
+
+export default Home2;
